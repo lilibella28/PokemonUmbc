54098f04dd2b5b22c87f3e24294daf3094050822
lilibella <lilibella28@hotmail.com>
2024-06-25 20:38:28 -0400

    New file called testing_data.php for testing the data

---
+++ src/testing_data.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "pokemon";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // Fetch and display Name and ID from the database
+        $sql = "SELECT ID, Name FROM pokemon_data";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        echo "<ul>";
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            echo "<li>";
+            echo "ID: " . ($row['ID']) . "<br>";
+            echo "Name: " . ($row['Name']) . "<br>";
+            echo "</li>";
+        }
+    } catch (PDOException $e) {
+        echo "<p>connection failed: " . $e->getMessage() . "</p>";
+        die();
+    } catch (Exception $e) {
+        echo "<p>error: " . $e->getMessage() . "</p>";
+    }
+    ?>
+</body>
+
+</html>
