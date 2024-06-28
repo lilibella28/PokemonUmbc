91864b6ab8dbb04187317ff5dfcce28d800469e9
Rajshree Pani <rajshreepani@Rajshrees-Air.fios-router.home>
2024-06-27 20:37:19 -0400

    removed task 3

--- database/create_database.sql
+++ database/create_database.sql
@@ -43,30 +43,3 @@ CREATE TABLE IF NOT EXISTS WildEncounters (
     EncounterRate DECIMAL(5, 2) NOT NULL CHECK (EncounterRate >= 0 AND EncounterRate <= 100),
     FOREIGN KEY (PokemonID) REFERENCES pokemon_data(ID)
 );
-
-CREATE TABLE IF NOT EXISTS Evolution (
-    PokemonID INT NOT NULL,
-    EvolutionLevel INT,
-    EvolutionMethod VARCHAR(50),
-    EvolvedFormID INT NOT NULL,
-    PRIMARY KEY (PokemonID, EvolvedFormID),
-    FOREIGN KEY (PokemonID) REFERENCES pokemon_data(PokemonID),
-    FOREIGN KEY (EvolvedFormID) REFERENCES pokemon_data(PokemonID)
-);
-
-CREATE TABLE IF NOT EXISTS NPCTeams (
-    NPCID INT NOT NULL,
-    PokemonSlot INT NOT NULL CHECK (PokemonSlot BETWEEN 1 AND 6),
-    PokemonID INT NOT NULL,
-    Level INT NOT NULL,
-    HP INT NOT NULL,
-    Attack INT NOT NULL,
-    Defense INT NOT NULL,
-    SpecialAttack INT NOT NULL,
-    SpecialDefense INT NOT NULL,
-    Speed INT NOT NULL,
-    Move1 VARCHAR(50),
-    Move2 VARCHAR(50),
-    PRIMARY KEY (NPCID, PokemonSlot),
-    FOREIGN KEY (PokemonID) REFERENCES pokemon_data(PokemonID)
-);
