c3a412a4a2cd79716f3c85776eab04bbb9d45bd9
lilibella <lilibella28@hotmail.com>
2024-07-03 17:04:56 -0400

    working on image

--- image_testing.php
+++ image_testing.php
@@ -8,20 +8,23 @@
 </head>
 <body>
     <?php
-        $folder_image = "../proj3_images/1st_Generation";
+    include './proj3_images/1st_Generation';
+        $folder_image = "./proj3_images/1st_Generation";
         $dir_handle = opendir($folder_image);
         if ($dir_handle) {
             while (($file = readdir($dir_handle)) !== false) {
                 if ($file != '.' && $file != '..') {
                 
-                    echo '<img src="../proj3_images/1st_Generation/' . $file . '" alt="' . $file . '" />';
+                    echo '<img src="./proj3_images/1st_Generation/' . $file . '" alt="' . $file . '" />';
                 }
             }
             closedir($dir_handle);
         } else {
             echo "Could not open directory.";
         }
+        
    
     ?>
+   
 </body>
 </html>
